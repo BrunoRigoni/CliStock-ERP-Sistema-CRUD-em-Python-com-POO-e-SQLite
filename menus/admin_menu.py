63394afe12a32633ca01455ebc9dbd8ea26d5854
BrunoRigoni <brigoni2011@gmail.com>
2025-07-02 16:34:19 -0300

    feat: implementar atualização e remoção de produtos no estoque
    - Adicionada interface para atualizar nome, estoque e preço dos produtos cadastrados
    - Inclusa validação de inputs e tratamento de erros para garantir dados válidos
    - Implementada função de remoção de produtos por ID com confirmação
    - Melhorias na experiência do usuário com feedbacks claros e controle de fluxo

--- menus/admin_menu.py
+++ menus/admin_menu.py
@@ -47,24 +47,44 @@ class storageDAO:
             "SELECT id, name, quantity, price FROM products WHERE user_id = ?", (user_id,))
         return self.cursor.fetchall()
 
-    def update_product_name(self, id, product):
+    def update_product_name(self, id, new_name):
         self.cursor.execute(
-            "UPDATE products SET name = ? WHERE id = ?", (product.name, id))
+            "UPDATE products SET name = ? WHERE id = ?", (new_name, id))
         self.connect.commit()
 
-    def update_product_quantity(self, id, product):
+    def update_product_quantity(self, id, new_quantity):
         self.cursor.execute(
-            "UPDATE products SET quantity = ? WHERE id = ?", (product.quantity, id))
+            "UPDATE products SET quantity = ? WHERE id = ?", (new_quantity, id))
         self.connect.commit()
 
-    def update_product_price(self, id, product):
+    def update_product_price(self, id, new_price):
         self.cursor.execute(
-            "UPDATE products SET price = ? WHERE id = ?", (product.price, id))
+            "UPDATE products SET price = ? WHERE id = ?", (new_price, id))
         self.connect.commit()
 
     def remove(self, id):
         self.cursor.execute("DELETE FROM products WHERE id = ?", (id,))
         self.connect.commit()
+
+
+def detailed_products(user_id, products):
+    total_price = 0
+    print("\nID | NOME | QUANTIDADE | PREÇO")
+    for p in products:
+        subtotal = p[2] * p[3]
+        total_price += subtotal
+        print(
+            f"{p[0]} | {p[1]} | {p[2]} UN | R$ {p[3]:.2f} | R$ {str(f'{subtotal:.2f}').replace('.', ',')}")
+    print("-" * 50)
+    print(
+        f"TOTAL GERAL DO ESTOQUE DO ADM ({user_id}): R$ {str(f'{total_price:.2f}').replace('.', ',')}")
+
+
+def simple_products(user_id, products):
+    print("\nID | NOME | QUANTIDADE | PREÇO")
+    for p in products:
+        print(
+            f"{p[0]} | {p[1]} | {p[2]} UN | R$ {str(f'{p[3]:.2f}').replace('.', ',')}")
 
 
 def admin_menu(user_id):
@@ -102,18 +122,7 @@ def admin_menu(user_id):
             if not products:
                 print("Nenhum produto cadastrado!")
             else:
-                total_price = 0
-                print("\nID | NOME | QUANTIDADE | PREÇO")
-                for p in products:
-                    subtotal = p[2] * p[3]
-                    total_price += subtotal
-
-                    print(
-                        f"{p[0]} | {p[1]} | {p[2]} UN | R$ {p[3]:.2f} | R$ {subtotal:.2f}")
-
-                    print("-" * 50)
-                    print(
-                        f"TOTAL GERAL DO ESTOQUE DO ADM ({user_id}): R$ {total_price:.2f}")
+                simple_products(user_id, products)
                 pause()
                 clear()
         elif sub_option == 3:
@@ -125,11 +134,99 @@ def admin_menu(user_id):
 [3]ATUALIZAR PREÇO
 [4] SAIR
                       """)
-                update_options = int(input("DIGITE A OPÇÃO: "))
+                products = storage_DAO.show_products(user_id)
+                try:
+                    update_options = int(input("DIGITE A OPÇÃO: "))
+                except ValueError:
+                    print("Digite uma opção válida.")
+
+                if update_options == 1:
+                    clear()
+
+                    if not products:
+                        print("Nenhum produto cadastrado!")
+                    else:
+                        simple_products(user_id, products)
+
+                        try:
+                            id_update = int(
+                                input("ID do produto que deseja alterar o nome: "))
+                        except ValueError:
+                            print("Digite um ID válido.")
+                            continue
+                        name_update = input("NOVO NOME: ")
+                        storage_DAO.update_product_name(name_update, id_update)
+                        print(
+                            f"O nome do produto ID {id_update} foi alterado para -> {name_update}")
+                        pause()
+                elif update_options == 2:
+                    clear()
+                    if not products:
+                        print("Nenhum produto cadastrado!")
+                    else:
+                        simple_products(user_id, products)
+                        try:
+                            id_update = int(
+                                input("ID do produto que deseja alterar o estoque: "))
+                        except ValueError:
+                            print("Digite um ID válido.")
+                            continue
+                        storage_update = int(
+                            input("NOVA QUANTIDADE EM ESTOQUE: "))
+                        storage_DAO.update_product_quantity(
+                            id_update, storage_update, )
+                        print(
+                            f"O estoque do produto ID {id_update} foi alterado para -> {storage_update}un")
+                        pause()
+
+                elif update_options == 3:
+                    clear()
+                    if not products:
+                        print("Nenhum produto cadastrado!")
+                    else:
+                        simple_products(user_id, products)
+                        try:
+                            id_update = int(
+                                input("ID do produto que deseja alterar o estoque: "))
+                        except ValueError:
+                            print("Digite um ID válido.")
+                            continue
+                        price_update = float(
+                            input("NOVO VALOR: ").replace(",", "."))
+                        storage_DAO.update_product_price(
+                            id_update, price_update, )
+                        print(
+                            f"O preço do produto -> ID {id_update} foi alterado para -> {price_update:.2f} R$")
+                        pause()
+
+                elif update_options == 4:
+                    print("Voltando...")
+                    pause()
+                    break
+
+                else:
+                    print("Digite uma opção válida.")
+                    pause()
+        elif sub_option == 4:
+            clear()
+            products = storage_DAO.show_products(user_id)
+
+            if not products:
+                print("Nenhum produto cadastrado!")
+            else:
+                simple_products(user_id, products)
+                try:
+                    id_delete = int(
+                        input("ID do produto que deseja deletar: "))
+                except ValueError:
+                    print("Digite um ID válido.")
+                else:
+                    storage_DAO.remove(id_delete)
+                    print(f"Produto com ID {id_delete} removido com sucesso.")
 
         elif sub_option == 5:
             print("SAINDO")
-            quit()
+            break
 
         else:
             print("DIGITE UMA OPÇÃO VÁLIDA!")
